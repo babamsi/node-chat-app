fadb00df9f449f885f9fe971a6c968f063d14782
suhaybcabdulahi <sabkacade40@gmail.com>
2019-09-02 05:36:30 -0400

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ io.on('connection', (socket) => {
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined the app'))
 
   socket.on('createMessage', (message) => {
-    console.log('createMessage', message);
+    
     io.emit('newMessage', generateMessage(message.from, message.text))
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
